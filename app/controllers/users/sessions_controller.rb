ea8ab2d7093d4979ed64446421e95a153f093dd4
hogino <hogino1003@gmail.com>
2015-02-14 11:50:02 +0900

    devise deleted

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  # exclude action :new to register directive
-  prepend_before_filter :require_no_authentication, only: [ :create ]
-
-  # layout false
-end
